13362f0c6323dcd7f3a8cefccda1d0545669510e
Meowinosis <ntphon37@gmail.com>
2024-01-18 17:20:55 +0700

    api search house

--- src/main/java/vn/codegym/houserental/service/HouseService.java
+++ src/main/java/vn/codegym/houserental/service/HouseService.java
@@ -3,6 +3,9 @@ package vn.codegym.houserental.service;
 import vn.codegym.houserental.model.House;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import vn.codegym.houserental.requests.SearchRequest;
+
+import java.util.List;
 
 public interface HouseService extends IGenerateService<House> {
     Page<House> findAll(Pageable pageable);
@@ -10,4 +13,5 @@ public interface HouseService extends IGenerateService<House> {
     Page<House> findAllByCategoryId(Pageable pageable, Long categoriesId);
     Iterable<House> findAllByUserIdAndDeleteFlag(Long userId,boolean deleteFlag);
     Iterable<House> findByNameContainsIgnoreCaseAndDeleteFlag(String name,boolean deleteFlag);
+    List<House> findByCondition(SearchRequest request);
 }
